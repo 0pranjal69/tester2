c82709714ef12f409a50ad402c5e6e42a1749bd0
0pranjal69 <147161375+0pranjal69@users.noreply.github.com>
2025-08-05 21:47:24 +0530

    Ready frontend for Vercel deployment

--- root/src/component/Contact.jsx
+++ root/src/component/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
     const data = Object.fromEntries(formData.entries());
 
     try {
-     const res = await axios.post("http://localhost:5000/send", data);
+     const res = await axios.post("https://tester2-4h76.onrender.com", data);
 
       if (res.data.success) {
         toast.success("Email sent successfully!");
